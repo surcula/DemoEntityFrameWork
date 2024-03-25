8b209937ebe7952e87b6fa56914d5360fdb9bca9
david gaillet <david.gaillet@live.fr>
2024-03-25 16:31:19 +0100

    Maj

--- Program.cs
+++ Program.cs
@@ -1,24 +1,39 @@
 ﻿using DemoEntityFrameWork.Contexts;
 using DemoEntityFrameWork.Entities;
+using DemoEntityFrameWork.Repositories;
 
 using (DemoContext context = new DemoContext())
 {
-    Person person = new Person()
-    {
-        FirstName = "David",
-        LastName = "Gaillet",
-        Pseudo = "Surcula",
-        Email = "bbbb"
-    };
-    Person person2 = new Person()
-    {
-        FirstName = "Dark",
-        LastName = "Vador",
-        Pseudo = "Seigneur Noir",
-        Email = "66@deathstar.co"
-    };
-    context.Persons.Add(person);
-    context.Persons.Add(person2);
-    context.SaveChanges();
-   
+
+    //Ajouter des données dans la table
+    //Person person = new Person()
+    //{
+    //    FirstName = "David",
+    //    LastName = "Gaillet",
+    //    Pseudo = "Surcula",
+    //    Email = "bbbb"
+    //};
+    //Person person2 = new Person()
+    //{
+    //    FirstName = "Dark",
+    //    LastName = "Vador",
+    //    Pseudo = "Seigneur Noir",
+    //    Email = "66@deathstar.co"
+    //};
+    //context.Persons.Add(person);
+    //context.Persons.Add(person2);
+    //context.SaveChanges();
+
+    //Recherche tous les éléments
+   //IEnumerable<Person> people = context.Persons.ToList();
+   // foreach (var item in people)
+   // {
+   //     Console.WriteLine($"{item.LastName} {item.FirstName} {item.Pseudo} {item.Email}");
+   // }
+
+
 }
+
+
+PersonRepository personRepository = new PersonRepository();
+Person p3 = personRepository.GetOne(p => p.Id == 2);
---
+++ Repositories/PersonRepository.cs
@@ -0,0 +1,82 @@
+﻿using DemoEntityFrameWork.Contexts;
+using DemoEntityFrameWork.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoEntityFrameWork.Repositories
+{
+    public class PersonRepository
+    {
+        public IEnumerable<Person> getAll()
+        {
+            using (DemoContext context = new DemoContext())
+            {
+                return context.Persons;
+            }
+        }
+        public Person? GetOne(Func<Person, bool> predicate)
+        {
+            using (DemoContext context = new DemoContext())
+            {
+                return context.Persons.FirstOrDefault(predicate);
+            }
+        }
+
+        public IEnumerable<Person> GetMany(Func<Person, bool> predicate)
+        {
+            using (DemoContext context = new DemoContext())
+            {
+                return context.Persons.Where(predicate);
+            }
+        }
+
+        public Person Insert(Person person)
+        {
+            using (DemoContext context = new DemoContext())
+            {
+                Person result = context.Persons.Add(person).Entity;
+                context.SaveChanges();
+                return result;
+            }
+        }
+        public Person Update(int id, Person person)
+        {
+            using (DemoContext context = new DemoContext())
+            {
+                Person? existingPerson = context.Persons.FirstOrDefault(p => p.Id == id);
+                if (existingPerson is null)
+                {
+                    throw new Exception();
+                }
+                existingPerson.FirstName = person.FirstName;
+                existingPerson.LastName = person.LastName;
+                existingPerson.Pseudo = person.Pseudo;
+                existingPerson.Email = person.Email;
+                context.SaveChanges();
+                return existingPerson;
+            }
+        }
+        public void Delete(int id)
+        {
+
+            using (DemoContext context = new DemoContext())
+            {
+               
+                Person? existingPerson = context.Persons.SingleOrDefault(p => p.Id == id);
+                if (existingPerson is null)
+                {
+                    throw new Exception("This person doesn't exist");
+                }
+                context.Remove(existingPerson);
+                context.SaveChanges();
+            }
+
+        }
+    }
+
+
+}
